1e7a5e77f5044abb6e9b8b8fabf59d8c0f09e09b
millatip <pratiwimillati@gmail.com>
2019-01-07 16:15:37 +0700

    init

---
+++ app/src/main/java/com/example/caribug/MainActivity.java
@@ -0,0 +1,30 @@
+
+package com.example.caribug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private EditText edt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt = findViewById(R.id.edt);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello " + edt.getText().toString(),Toast.LENGTH_SHORT ).show();
+            }
+        });
+    }
+}
